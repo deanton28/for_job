601a93ba03b319031ae0245f403961f427c3441e
deanton28 <deanton28@gmail.com>
2020-02-11 15:38:11 +0300

    started work whith js

---
+++ video_edu/js/carousel.js
@@ -0,0 +1,7 @@
+console.log('check');
+
+const carouselArea = document.querySelector('.carousel_wrapper');
+console.log(carouselArea.innerHTML);
+
+const iconsList = carouselArea.querySelectorAll('.icon');
+console.log(iconsList.length);
