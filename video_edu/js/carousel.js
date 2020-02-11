c743ae442f6311aa7ee237bcd6b4890d78759d43
deanton28 <deanton28@gmail.com>
2020-02-12 02:00:43 +0300

    add action on arrow left and right

--- video_edu/js/carousel.js
+++ video_edu/js/carousel.js
@@ -1,7 +1,24 @@
 console.log('check');
 
 const carouselArea = document.querySelector('.carousel_wrapper');
-console.log(carouselArea.innerHTML);
+const iconsArea = carouselArea.querySelector('.icons_wrapper');
+let iconsList = iconsArea.querySelectorAll('.icon');
+console.log(iconsList);
+const arrowRight = carouselArea.querySelector('.arrow-right');
+const arrowLeft = carouselArea.querySelector('.arrow-left');
 
-const iconsList = carouselArea.querySelectorAll('.icon');
-console.log(iconsList.length);
+arrowRight.addEventListener('click', () => {
+  iconsArea.querySelector('.active').classList.remove('active');
+  iconsArea.insertBefore(iconsList[2], iconsList[0]);
+  iconsList = iconsArea.querySelectorAll('.icon');
+  console.log(iconsArea);
+  iconsList[1].classList.add('active');
+});
+
+arrowLeft.addEventListener('click', () => {
+  iconsArea.querySelector('.active').classList.remove('active');
+  iconsArea.appendChild(iconsList[0]);
+  iconsList = iconsArea.querySelectorAll('.icon');
+  console.log(iconsArea);
+  iconsList[1].classList.add('active');
+});
