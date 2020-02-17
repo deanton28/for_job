af8ca2bc70984a7d826bbd37416e344556eb9490
deanton28 <deanton28@gmail.com>
2020-02-17 16:27:34 +0300

    current work

--- video_edu/js/carousel.js
+++ video_edu/js/carousel.js
@@ -16,9 +16,13 @@ arrowRight.addEventListener('click', () => {
 });
 
 arrowLeft.addEventListener('click', () => {
-  iconsArea.querySelector('.active').classList.remove('active');
-  iconsArea.appendChild(iconsList[0]);
-  iconsList = iconsArea.querySelectorAll('.icon');
-  console.log(iconsArea);
-  iconsList[1].classList.add('active');
+  iconsArea.appendChild(iconsList[0].cloneNode(true));
+  setTimeout(() => {
+    iconsList = iconsArea.querySelectorAll('.icon');
+    iconsArea.querySelector('.active').classList.remove('active');
+    for (let i = 0; i < iconsList.length; i += 1) {
+      iconsList[i].classList.add('move-left');
+    }
+    iconsList[2].classList.add('active');
+  }, 1000);
 });
