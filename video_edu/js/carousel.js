ffdd9c847d50f4c3f87c44d5b619e420f5577c47
deanton28 <deanton28@gmail.com>
2020-02-18 01:29:13 +0300

    carousel finish, need style update

--- video_edu/js/carousel.js
+++ video_edu/js/carousel.js
@@ -8,21 +8,35 @@ const arrowRight = carouselArea.querySelector('.arrow-right');
 const arrowLeft = carouselArea.querySelector('.arrow-left');
 
 arrowRight.addEventListener('click', () => {
-  iconsArea.querySelector('.active').classList.remove('active');
-  iconsArea.insertBefore(iconsList[2], iconsList[0]);
+  const newElement = iconsList[2].cloneNode(true);
+  newElement.classList.add('move-left');
+  console.log(newElement);
+  iconsArea.insertBefore(newElement, iconsList[0]);
   iconsList = iconsArea.querySelectorAll('.icon');
-  console.log(iconsArea);
-  iconsList[1].classList.add('active');
+  // iconsList[0].classList.add('move-left');
+  console.log(iconsList);
+  setTimeout(() => {
+    iconsList[0].classList.remove('move-left');
+    iconsList[1].classList.add('active');
+    iconsList[2].classList.remove('active');
+    setTimeout(() => {
+      iconsList[3].remove();
+      iconsList = iconsArea.querySelectorAll('.icon');
+      console.log(iconsList);
+    }, 500);
+  }, 30);
 });
 
 arrowLeft.addEventListener('click', () => {
   iconsArea.appendChild(iconsList[0].cloneNode(true));
+  iconsList[0].classList.add('move-left');
+  iconsList[1].classList.remove('active');
+  iconsList[2].classList.add('active');
+  iconsList = iconsArea.querySelectorAll('.icon');
+  console.log(iconsList);
   setTimeout(() => {
+    iconsList[0].remove();
     iconsList = iconsArea.querySelectorAll('.icon');
-    iconsArea.querySelector('.active').classList.remove('active');
-    for (let i = 0; i < iconsList.length; i += 1) {
-      iconsList[i].classList.add('move-left');
-    }
-    iconsList[2].classList.add('active');
-  }, 1000);
+    console.log(iconsList);
+  }, 500);
 });
