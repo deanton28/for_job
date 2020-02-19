07ad781bc346cada12be6241cc77552ff11bba1e
ANTON DEVUSHKIN <30399939+deanton28@users.noreply.github.com>
2020-02-19 12:51:33 +0300

    Add files via upload

---
+++ help_edu/js/disp_help_discr.js
@@ -0,0 +1,18 @@
+﻿console.log('check');
+
+const helpArea = document.querySelector('.help_wrapper');
+console.log(helpArea);
+const helpIcon = helpArea.querySelector('.help-icon');
+console.log(helpIcon);
+const helpDescription = helpArea.querySelector('.help-description');
+console.log(helpDescription);
+
+helpIcon.addEventListener('click', () => {
+  if (helpIcon.classList.contains('close')) {
+    helpIcon.classList.remove('close');
+    helpDescription.setAttribute('hidden', 'hidden');
+  } else {
+    helpIcon.classList.add('close');
+    helpDescription.removeAttribute('hidden');
+  }
+});
