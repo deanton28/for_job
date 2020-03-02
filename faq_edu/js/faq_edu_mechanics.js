51e8ce4ac39207e5254bc23db5c97e12986ba557
deanton28 <deanton28@gmail.com>
2020-03-03 01:12:12 +0300

    unit faq updated, change mechanics

--- faq_edu/js/faq_edu_mechanics.js
+++ faq_edu/js/faq_edu_mechanics.js
@@ -3,48 +3,41 @@ console.log('check');
 const workPlace = document.querySelector('.faq-edu-wrapper');
 const coiceBlockArea = workPlace.querySelector('.choice-block-area');
 console.log(coiceBlockArea);
-const arrowLeft = coiceBlockArea.querySelector('.arrow-left');
-const arrowRight = coiceBlockArea.querySelector('.arrow-right');
-// const blockArea = coiceBlockArea.querySelector('.block-area');
-const blockNameList = coiceBlockArea.querySelectorAll('.block-name');
-console.log(blockNameList);
-const blockQuestionList = workPlace.querySelectorAll('.question-block');
-console.log(blockQuestionList.length);
-let blockCount = 0;
+const blockList = workPlace.querySelectorAll('.block-name');
+console.log(blockList);
+const questionBlockList = workPlace.querySelectorAll('.question-block');
+console.log(questionBlockList);
+const questionList = workPlace.querySelectorAll('.question');
+console.log(questionList);
+const answerList = workPlace.querySelectorAll('.answer');
+console.log(answerList);
 
-arrowLeft.addEventListener('click', () => {
-  console.log('arrowLeft check');
-  if (blockCount > 0) {
-    for (let i = 0; i < blockNameList.length; i += 1) {
-      blockNameList[i].classList.remove('active');
+for (let i = 0; i < blockList.length; i += 1) {
+  blockList[i].addEventListener('click', (evt) => {
+    console.log(evt.target.dataset.target);
+    for (let q = 0; q < blockList.length; q += 1) {
+      blockList[q].classList.remove('active');
     }
-    blockCount -= 1;
-    console.log(blockCount);
-    blockNameList[blockCount].classList.add('active');
-    console.log(blockNameList);
-    for (let q = 0; q < blockQuestionList.length; q += 1) {
-      blockQuestionList[q].setAttribute('hidden', 'hidden');
+    blockList[i].classList.add('active');
+    const target = workPlace.querySelector(evt.target.dataset.target);
+    console.log(target);
+    for (let a = 0; a < answerList.length; a += 1) {
+      answerList[a].classList.remove('visible');
     }
-    const questionsList = document.querySelector(blockNameList[blockCount].dataset.target);
-    console.log(questionsList);
-    questionsList.removeAttribute('hidden');
-  }
-});
+    for (let e = 0; e < questionBlockList.length; e += 1) {
+      questionBlockList[e].setAttribute('hidden', 'hidden');
+    }
+    target.removeAttribute('hidden');
+  });
+}
 
-arrowRight.addEventListener('click', () => {
-  if (blockCount < blockNameList.length - 1) {
-    for (let i = 0; i < blockNameList.length; i += 1) {
-      blockNameList[i].classList.remove('active');
-    }
-    blockCount += 1;
-    console.log(blockCount);
-    blockNameList[blockCount].classList.add('active');
-    console.log(blockNameList);
-    for (let q = 0; q < blockQuestionList.length; q += 1) {
-      blockQuestionList[q].setAttribute('hidden', 'hidden');
-    }
-    const questionsList = document.querySelector(blockNameList[blockCount].dataset.target);
-    console.log(questionsList);
-    questionsList.removeAttribute('hidden');
-  }
-});
+for (let i = 0; i < questionList.length; i += 1) {
+  questionList[i].addEventListener('click', (evt) => {
+    console.log(evt.target.dataset.target);
+    const target = workPlace.querySelector(evt.target.dataset.target);
+    console.log(target);
+    console.log(target.hidden);
+
+    target.classList.toggle('visible');
+  });
+}
