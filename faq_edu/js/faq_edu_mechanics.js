9209217dce8c5fe2ec2b64560e9f4f5d1562844e
deanton28 <deanton28@gmail.com>
2020-03-08 03:41:38 +0300

    add circle

--- faq_edu/js/faq_edu_mechanics.js
+++ faq_edu/js/faq_edu_mechanics.js
@@ -6,15 +6,24 @@ const questionBlockList = workPlace.querySelectorAll('.question-block');
 const questionList = workPlace.querySelectorAll('.question');
 const answerList = workPlace.querySelectorAll('.answer');
 
+for (question of questionList) {
+  console.log(question.innerHTML);
+  let circle = document.createElement('div');
+  circle.classList.add('circle');
+  circle.innerHTML = '<div class="circle_circle"><div class="circle_point"></div></div>';
+  question.appendChild(circle);
+}
+
 for (let i = 0; i < blockList.length; i += 1) {
   blockList[i].addEventListener('click', (evt) => {
+    console.log(evt.currentTarget);
     for (let q = 0; q < blockList.length; q += 1) {
       blockList[q].classList.remove('active');
     }
 
     blockList[i].classList.add('active');
 
-    const target = workPlace.querySelector(evt.target.dataset.target);
+    const target = workPlace.querySelector(evt.currentTarget.dataset.target);
 
     for (let a = 0; a < answerList.length; a += 1) {
       answerList[a].classList.remove('visible');
@@ -28,6 +37,9 @@ for (let i = 0; i < blockList.length; i += 1) {
     target.removeAttribute('hidden');
     setTimeout(() => {
       target.style.height = `${target.scrollHeight}px`;
+      setTimeout(() => {
+        target.style.height = '';
+      }, 1010);
     }, 10);
     console.log(target.attributes);
     console.log(target);
@@ -37,6 +49,14 @@ for (let i = 0; i < blockList.length; i += 1) {
 for (let i = 0; i < questionList.length; i += 1) {
   questionList[i].addEventListener('click', (evt) => {
     const target = workPlace.querySelector(evt.target.dataset.target);
+    target.style.height = `${target.scrollHeight}px`;
     target.classList.toggle('visible');
+
+    setTimeout(() => {
+      target.style.height = `${target.scrollHeight}px`;
+      setTimeout(() => {
+        target.style.height = '';
+      }, 1010);
+    }, 10);
   });
 }
