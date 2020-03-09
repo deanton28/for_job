46e487438039e425d06759ec04d931cb7db30b6e
ANTON DEVUSHKIN <30399939+deanton28@users.noreply.github.com>
2020-03-10 00:57:14 +0300

    Faq edu (#38)
    * spelling errors fixed
    
    * add 3 questions
    
    * unit faq updated, change mechanics
    
    * style updates (label)
    
    * updatet 3 questions
    
    * updatet style
    
    * add animation to display questions list
    
    * add circle
    
    * style updated

--- faq_edu/js/faq_edu_mechanics.js
+++ faq_edu/js/faq_edu_mechanics.js
@@ -1,50 +1,63 @@
-console.log('check');
-
 const workPlace = document.querySelector('.faq-edu-wrapper');
 const coiceBlockArea = workPlace.querySelector('.choice-block-area');
 console.log(coiceBlockArea);
-const arrowLeft = coiceBlockArea.querySelector('.arrow-left');
-const arrowRight = coiceBlockArea.querySelector('.arrow-right');
-// const blockArea = coiceBlockArea.querySelector('.block-area');
-const blockNameList = coiceBlockArea.querySelectorAll('.block-name');
-console.log(blockNameList);
-const blockQuestionList = workPlace.querySelectorAll('.question-block');
-console.log(blockQuestionList.length);
-let blockCount = 0;
+const blockList = workPlace.querySelectorAll('.block-name');
+const questionBlockList = workPlace.querySelectorAll('.question-block');
+const questions = workPlace.querySelectorAll('.questions-list > li');
+const questionList = workPlace.querySelectorAll('.question');
+const answerList = workPlace.querySelectorAll('.answer');
 
-arrowLeft.addEventListener('click', () => {
-  console.log('arrowLeft check');
-  if (blockCount > 0) {
-    for (let i = 0; i < blockNameList.length; i += 1) {
-      blockNameList[i].classList.remove('active');
-    }
-    blockCount -= 1;
-    console.log(blockCount);
-    blockNameList[blockCount].classList.add('active');
-    console.log(blockNameList);
-    for (let q = 0; q < blockQuestionList.length; q += 1) {
-      blockQuestionList[q].setAttribute('hidden', 'hidden');
-    }
-    const questionsList = document.querySelector(blockNameList[blockCount].dataset.target);
-    console.log(questionsList);
-    questionsList.removeAttribute('hidden');
-  }
-});
+for (question of questions) {
+  console.log(question.innerHTML);
+  let circle = document.createElement('div');
+  circle.classList.add('circle');
+  circle.innerHTML = '<div class="circle_circle"><div class="circle_point"></div></div>';
+  question.appendChild(circle);
+}
 
-arrowRight.addEventListener('click', () => {
-  if (blockCount < blockNameList.length - 1) {
-    for (let i = 0; i < blockNameList.length; i += 1) {
-      blockNameList[i].classList.remove('active');
+for (let i = 0; i < blockList.length; i += 1) {
+  blockList[i].addEventListener('click', (evt) => {
+    console.log(evt.currentTarget);
+    for (let q = 0; q < blockList.length; q += 1) {
+      blockList[q].classList.remove('active');
     }
-    blockCount += 1;
-    console.log(blockCount);
-    blockNameList[blockCount].classList.add('active');
-    console.log(blockNameList);
-    for (let q = 0; q < blockQuestionList.length; q += 1) {
-      blockQuestionList[q].setAttribute('hidden', 'hidden');
+
+    blockList[i].classList.add('active');
+
+    const target = workPlace.querySelector(evt.currentTarget.dataset.target);
+
+    for (let a = 0; a < answerList.length; a += 1) {
+      answerList[a].classList.remove('visible');
     }
-    const questionsList = document.querySelector(blockNameList[blockCount].dataset.target);
-    console.log(questionsList);
-    questionsList.removeAttribute('hidden');
-  }
-});
+
+    for (let e = 0; e < questionBlockList.length; e += 1) {
+      questionBlockList[e].style.height = '0';
+      questionBlockList[e].setAttribute('hidden', 'hiden');
+    }
+
+    target.removeAttribute('hidden');
+    setTimeout(() => {
+      target.style.height = `${target.scrollHeight}px`;
+      setTimeout(() => {
+        target.style.height = '';
+      }, 500);
+    }, 10);
+    console.log(target.attributes);
+    console.log(target);
+  });
+}
+
+for (let i = 0; i < questionList.length; i += 1) {
+  questionList[i].addEventListener('click', (evt) => {
+    const target = workPlace.querySelector(evt.target.dataset.target);
+    target.style.height = `${target.scrollHeight}px`;
+    target.classList.toggle('visible');
+
+    setTimeout(() => {
+      target.style.height = `${target.scrollHeight}px`;
+      // setTimeout(() => {
+      //   target.style.height = '';
+      // }, 4000);
+    }, 10);
+  });
+}
