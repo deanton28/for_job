ad39786e183a460a5717af055c353ef9e7d89cbe
ANTON DEVUSHKIN <30399939+deanton28@users.noreply.github.com>
2020-04-14 17:28:05 +0300

    delited console.log in js (#46)

--- help_edu/js/disp_help_discr.js
+++ help_edu/js/disp_help_discr.js
@@ -1,11 +1,6 @@
-﻿console.log('check');
-
 const helpArea = document.querySelector('.help_wrapper');
-console.log(helpArea);
 const helpIcon = helpArea.querySelector('.help-icon');
-console.log(helpIcon);
 const helpDescription = helpArea.querySelector('.help-description');
-console.log(helpDescription);
 
 helpIcon.addEventListener('click', () => {
   if (helpIcon.classList.contains('close')) {
